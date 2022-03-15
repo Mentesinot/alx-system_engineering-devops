e12e878e77c5ba51448149ac6bd740dbc558c929
Mentesinot <mentesinottegegn@gmail.com>
2022-03-15 11:33:12 -0500

    edit advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,13 @@
-# Configure the SSH server to authenticates only through SSH keys
-include stdlib
-
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-  match  => '^PasswordAuthentication'
+#Creates Sets up client SSH configuration file
+file_line { 'ssh_config':
+    ensure   => present,
+    path     => '/etc/ssh/ssh_config',
+    line     => 'PasswordAuthentication no',
+    multiple => 'true'
 }
-
-file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/holberton',
-  match  => '^IdentityFile'
+file_line { 'ssh_config_2':
+    ensure   => present,
+    path     => '/etc/ssh/ssh_config',
+    line     => 'IdentityFile ~/.ssh/school',
+    multiple => 'true'
 }
